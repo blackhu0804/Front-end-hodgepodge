bfcf1e8242864698b5a4dd18046d0a929b3c700f
hu970804 <812510003@qq.com>
2018-05-21 17:07:28 +0800

    add ArrowOffer

---
+++ JS编程/剑指offer/二维数组的查找.js
@@ -0,0 +1,20 @@
+/* 
+    在一个二维数组中，每一行都按照从左到右递增的顺序排序，
+    每一列都按照从上到下递增的顺序排序。
+    请完成一个函数，输入这样的一个二维数组和一个整数，判断数组中是否含有该整数。
+*/
+
+function Find(target, array) {
+    if(array.length > 0) {
+        for (let i = 0; i < array.length; i++) {
+            for (let j = 0; j < array[i].length; j++) {
+                if (target === array[i][j])
+                    return true
+            }
+        }
+    }
+    return false
+}
+
+
+
