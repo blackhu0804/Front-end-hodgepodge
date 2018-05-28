2a4c7f07f26856b048d59a49b9fbb45f41d8583a
hu970804 <812510003@qq.com>
2018-05-28 17:27:26 +0800

    求二进制中1的个数

--- JS编程/剑指offer/11.二进制中1的个数.js
+++ JS编程/剑指offer/11.二进制中1的个数.js
@@ -3,5 +3,22 @@
 */
 
 function NumberOf1(n) {
-    // write code here
+    if(typeof n !== 'number') {
+        return 
+    } else {
+        if (n < 0) {
+            n = n >>> 0
+        }
+
+        var twoNumber = n.toString(2)
+        console.log(twoNumber)
+        var count = 0
+        for(var i = 0; i < twoNumber.length;i++) {
+            if(twoNumber[i] === '1') {
+                count += 1
+            }
+        }
+        console.log(count)       
+    }
 }
+NumberOf1(1)
