65ccc78b33718c5bd272c1e8abfc35b946fbe0b9
胡天浩 <hutianhao@zuoyebang.com>
2019-07-26 20:21:25 +0800

    add leetcode 27-28

--- JS编程/leetcode-27.js
+++ JS编程/leetcode-27.js
@@ -1,15 +1,15 @@
 var strStr = function(haystack, needle) {
-    // if(needle.length === 0) return 0;
+    if(needle.length === 0) return 0;
 
-    return haystack.indexOf(needle);
-    // for(let i = 0; i < haystack.length - 1;i++) {
-    //   if(needle[0] === haystack[i]) {
-    //     if(needle === haystack.substr(i, needle.length - 1)) {
-    //       return i;
-    //     }
-    //   }
-    // }
-    // return -1;
+    if(haystack === needle) return 0;
+    for(let i = 0; i < haystack.length - 1;i++) {
+      if(needle[0] === haystack[i]) {
+        if(needle === haystack.substr(i, needle.length)) {
+          return i;
+        }
+      }
+    }
+    return -1;
 };
 
 strStr('hello', 'll');
