95bf47c228f57514c2f77b6b42e7287646df040c
hu970804 <812510003@qq.com>
2019-08-06 20:08:52 +0800

    add leetcode 66

---
+++ JS编程/leetcode-66.js
@@ -0,0 +1,18 @@
+/**
+ * @param {number[]} digits
+ * @return {number[]}
+ */
+var plusOne = function(digits) {
+  let len = digits.length;
+  for(let i = len - 1; i >= 0; i--) {
+    digits[i]++;
+    digits[i] %= 10;
+    if(digits[i] != 0) {
+      return digits;
+    }
+  }
+
+  digits = [...Array(len + 1)].map(item=>0);;
+  digits[0] = 1;
+  return digits;
+};
