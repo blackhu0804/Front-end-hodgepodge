e57a1c24e68584509c0dd24f39ea1a8322dc0a62
hu970804 <812510003@qq.com>
2020-01-09 22:33:18 +0800

    feat: add uncurry

--- ES6/currying.js
+++ ES6/currying.js
@@ -17,3 +17,15 @@ function curry(fn, ...args) {
 let fn = curry(add, 1, 2);
 let newFn = fn(3);
 console.log(newFn(4));
+
+
+// 反柯里化 让一个函数的应用范围变得更广一些
+function unCurrying(fn) {
+  return function (thisValue, ...args) {
+    return Function.prototype.apply.call(fn, thisValue, ...args);
+    // return fn.call(...args);
+  }
+}
+
+let toString = unCurrying(Object.prototype.toString);
+console.log(toString('hello'));
