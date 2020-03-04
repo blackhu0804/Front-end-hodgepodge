4169babc3b7cb661dd3f74db86c22e7b4cdd8390
hu970804 <812510003@qq.com>
2020-03-04 23:11:55 +0800

    feat: 文本编译实现

--- vue-source/source/vue/index.js
+++ vue-source/source/vue/index.js
@@ -1,4 +1,6 @@
 import {initState} from './observe';
+import Watcher from './observe/watcher';
+import {compiler, util} from './util';
 
 function Vue(options) { // Vue 中原始用户传入的数据
   this._init(options); // 初始化 Vue， 并且将用户选项传入
@@ -10,7 +12,55 @@ Vue.prototype._init = function(options) {
   vm.$options = options;
 
   // MVVM 原理， 需要数据重新初始化
+  // 拦截数组的方法 和 对象的属性
   initState(vm);
+
+  // 初始化工作
+  if (vm.$options.el) {
+    vm.$mount();
+  }
 }
 
+/**
+ * 获取DOM节点
+ * @param {*} el 
+ */
+function query(el) {
+  if (typeof el === 'string') {
+    return document.querySelector(el);
+  };
+  return el;
+}
+
+/**
+ * 用用户传入的数据，更新视图
+ */
+Vue.prototype._update = function() {
+  let vm = this;
+  let el = vm.$el;
+
+  /** TODO 虚拟DOM重写 */
+  // 匹配 {{}} 替换
+  let node = document.createDocumentFragment();
+  let firstChild;
+  while(firstChild = el.firstChild) {
+    node.appendChild(firstChild);
+  }
+
+  compiler(node, vm);
+
+  el.appendChild(node);
+}
+
+Vue.prototype.$mount = function() {
+  let vm = this;
+  let el = vm.$options.el; // 获取元素
+  el = vm.$el = query(el); // 获取当前挂载的节点 vm.$el 就是我要挂在的一个元素
+
+  // 渲染通过 watcher来渲染
+  let updateComponent = () => { // 更新、渲染的逻辑
+    vm._update(); // 更新组件
+  }
+  new Watcher(vm, updateComponent); // 渲染Watcher, 默认调用updateComponent
+}
 export default Vue
---
+++ vue-source/source/vue/util.js
@@ -0,0 +1,32 @@
+const defaultRE = /\{\{((?:.|\r?\n)+?)\}\}/g;
+export const util = {
+  getValue(vm, expr) { // school.name
+    let keys = expr.split('.');
+    return keys.reduce((memo, current) => {
+      memo = memo[current]; // 相当于 memo = vm.school.name
+      return memo;
+    }, vm);
+  },
+  /**
+   * 编译文本 替换{{}}
+   */
+  compilerText(node, vm) {
+    node.textContent = node.textContent.replace(defaultRE, function(...args) {
+      return util.getValue(vm, args[1]);
+    });
+  }
+}
+
+/**
+ * 文本编译
+ */
+export function compiler(node, vm) {
+  let childNodes = node.childNodes;
+  [...childNodes].forEach(child => { // 一种是元素一种是文本
+    if (child.nodeType == 1) { // 1表示元素 3表示文本
+      compiler(child, vm); // 递归编译当前元素的孩子节点
+    } else if (child.nodeType == 3) {
+      util.compilerText(child, vm);
+    }
+  })
+}
