d65d87b523a4f8a70e511cfe6d4160d5a058b183
hu970804 <812510003@qq.com>
2020-03-23 18:30:12 +0800

    feat: leetcode 876

---
+++ JS编程/leetcode-876.js
@@ -0,0 +1,22 @@
+/**
+ * https://leetcode-cn.com/problems/middle-of-the-linked-list/
+ * 
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+var middleNode = function(head) {
+  let slow = head;
+  let fast = head;
+  while (fast && fast.next) {
+    slow = slow.next;
+    fast = fast.next.next;
+  }
+  return slow;
+};
