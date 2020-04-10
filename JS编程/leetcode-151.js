42f74eca3025d579368da51a606a9fe381e1df5f
hu970804 <812510003@qq.com>
2020-04-10 11:03:04 +0800

    feat: leetcode 151

---
+++ JS编程/leetcode-151.js
@@ -0,0 +1,9 @@
+/**
+ * @param {string} s
+ * @return {string}
+ */
+var reverseWords = function(s) {
+  return s.split(' ').filter(item => item !== '').reverse().join(' ');
+};
+
+console.log(reverseWords('  hello world!  '))
