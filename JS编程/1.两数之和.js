dc9813fe9675d7bc9823163042e41cbea17ab235
hutianhao <812510003@qq.com>
2020-06-26 15:13:47 +0800

    feat: leetcode 1

---
+++ JS编程/1.两数之和.js
@@ -0,0 +1,25 @@
+/*
+ * @lc app=leetcode.cn id=1 lang=javascript
+ *
+ * [1] 两数之和
+ */
+
+// @lc code=start
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+var twoSum = function(nums, target) {
+  for (let i = 0; i < nums.length; i++) {
+    for (let j = i + 1; j < nums.length; j++) {
+      if (nums[i] + nums[j] === target) {
+        return [i, j];
+      }
+    }
+  }
+};
+
+twoSum([3,2,4], 6)
+// @lc code=end
+
