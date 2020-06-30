72422a7b91eccba3ddff0ded12814966b1083d50
hutianhao <812510003@qq.com>
2020-06-30 20:23:06 +0800

    feat: leetcode 215

---
+++ JS编程/215.数组中的第k个最大元素.js
@@ -0,0 +1,20 @@
+/*
+ * @lc app=leetcode.cn id=215 lang=javascript
+ *
+ * [215] 数组中的第K个最大元素
+ */
+
+// @lc code=start
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {number}
+ */
+var findKthLargest = function(nums, k) {
+  const res = nums.sort((a, b) => {
+    return b - a;
+  })[k - 1];
+  return res;
+};
+// @lc code=end
+
