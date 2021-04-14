0e1a474279296b3a3209364aa359351e1765f5ef
blackhu0804 <812510003@qq.com>
2021-04-14 23:44:00 +0800

    feat: dp

--- JS编程/322.零钱兑换.js
+++ JS编程/322.零钱兑换.js
@@ -1,10 +1,3 @@
-/*
- * @lc app=leetcode.cn id=322 lang=javascript
- *
- * [322] 零钱兑换
- */
-
-// @lc code=start
 /**
  * @param {number[]} coins
  * @param {number} amount
@@ -12,17 +5,17 @@
  */
 var coinChange = function(coins, amount) {
   let dp = new Array(amount + 1).fill(Infinity);
-  dp[0] = 0;
 
-  for (let i = 0; i <= amount; i++) {
-    for(let coin of coins) {
-      if (i - coin >= 0) {
-        dp[i] = Math.min(dp[i], dp[i - coin] + 1);
+  dp[0] = 0;
+  for (let i = 0; i < amount; i++) {
+    for (let key of coins) {
+      if (i - key >= 0) {
+        dp[i] = Math.min(dp[i], dp[i - key] + 1);
       }
-    }    
+    }
   }
+  console.log(dp);
   return dp[amount] === Infinity ? -1 : dp[amount];
 };
 
-// @lc code=end
-
+console.log(coinChange([1, 2, 5], 11));
